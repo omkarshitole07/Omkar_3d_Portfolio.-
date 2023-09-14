de2c03449bbb7f831d3c8d719fa2d0f60cc5004c
omkarshitole07 <105565098+omkarshitole07@users.noreply.github.com>
2023-09-14 14:30:13 -0400

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+            I'm a software developer pursuing an MS in Computer Science (Software Engineering) at Pace University.
+            Proficient in Java, Python, HTML, CSS, and JavaScript, 
+            I excel in Agile, databases, and AWS Cloud architecture. As a Full Stack Software Dev Intern at ShopTaki, I've enhanced Web Applications & Mobile applications, 
+            Connected microservices, and optimized code. I've also driven projects in facial emotion recognition and social media apps using React Native. 
+            A Dataiku Hackathon's third-place win showcases my data analysis and ML skills. Ready to innovate in software development.
+            Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
